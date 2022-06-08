26efa13e26400aafa5b53eafaf432731fe738f55
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-06-07 23:46:55 -0600

    Add string changes and console.log integrations

--- variables.js
+++ variables.js
@@ -9,3 +9,11 @@ var siblings = 2
 var hungry = true
 var determined = true
 var eodSubmitted = false
+
+name = "Berto"
+location = "Detroit"
+hobby = "Photography"
+
+console.log(name)
+console.log(location)
+console.log(hobby)
